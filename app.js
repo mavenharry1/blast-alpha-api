302a497b36816dfc1477c3d235846640039b729e
maven harry <mavenharry@gmail.com>
2024-06-20 01:51:40 +0100

    feat: api setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const port = 3300;
+const port = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
